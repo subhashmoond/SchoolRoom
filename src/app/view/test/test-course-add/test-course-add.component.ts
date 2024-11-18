f13d86483c3986e3374733293e10df06690582b0
Ranjan Kumar <93971032+iamranjansah@users.noreply.github.com>
2024-11-18 19:42:03 +0530

    dfsdtest-course-add
    test-course-add

--- src/app/view/test/test-course-add/test-course-add.component.ts
+++ src/app/view/test/test-course-add/test-course-add.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators,   } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-test-course-add',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    CheckboxModule,],
   templateUrl: './test-course-add.component.html',
   styleUrl: './test-course-add.component.css'
 })
 export class TestCourseAddComponent {
+    
+  addTestForm!: FormGroup;
+  activeIndex: number = 0;
+  items: any;
+  courseList : any;
 
+  charCount: number = 0; // Initialize character count
+
+  constructor(private fb: FormBuilder) {
+    this.addTestForm = this.fb.group({
+      name: [''],
+      price: [''],
+      isFree: [false],
+    });
+  }
+
+  updateCharacterCount() {
+    this.charCount = this.addTestForm.get('name')?.value?.length || 0;
+  }
+
+  
+
+
+  next() {
+    if (this.activeIndex < this.items.length - 1) {
+      this.activeIndex++;
+    }
+  }
+
+  back() {
+    if (this.activeIndex > 0) {
+      this.activeIndex--;
+    }
+  }
+
+ 
+
+  // addTestForm: FormGroup;
+  
+
+  
+ 
 }
