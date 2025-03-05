868cca727816a70e515e07358e282e2a156eacc3
Subhash Moond <subhash@kugelblitz.xyz>
2025-03-05 09:42:44 +0530

    report chart design

--- src/app/shared/components/chart/line-chart/line-chart.component.ts
+++ src/app/shared/components/chart/line-chart/line-chart.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+
+import { NgApexchartsModule } from "ng-apexcharts";
+import {
+  ApexChart,
+  ApexAxisChartSeries,
+  ChartComponent,
+  ApexXAxis
+} from "ng-apexcharts";
+import { CardModule } from 'primeng/card';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+};
 
 @Component({
   selector: 'app-line-chart',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, CardModule, NgApexchartsModule ],
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.css'
 })
 export class LineChartComponent {
+
+   @ViewChild("chart") chart : ChartComponent | undefined;
+          public chartOptions: ChartOptions;
+        
+          constructor() {
+            this.chartOptions = {
+              series: [
+                {
+                  name: "My Series",
+                  data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+                }
+              ],
+              chart: {
+                type: "line",
+                toolbar: {
+                  show: false
+                },
+                height: 350
+              },
+              
+              xaxis: {
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+              }
+            };
+          }
+    
 
 }
--- src/app/view/report/overview-report/overview-report.component.ts
+++ src/app/view/report/overview-report/overview-report.component.ts
@@ -1,53 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { NgApexchartsModule } from "ng-apexcharts";
-import {
-  ApexChart,
-  ApexAxisChartSeries,
-  ChartComponent,
-  ApexXAxis
-} from "ng-apexcharts";
-
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-};
-
+import { BarChartComponent } from '../../../shared/components/chart/bar-chart/bar-chart.component';
+import { LineChartComponent } from '../../../shared/components/chart/line-chart/line-chart.component';
 @Component({
   selector: 'app-overview-report',
   standalone: true,
-  imports: [ CommonModule, CardModule, NgApexchartsModule ],
+  imports: [CommonModule, CardModule, BarChartComponent, LineChartComponent],
   templateUrl: './overview-report.component.html',
   styleUrl: './overview-report.component.css'
 })
 export class OverviewReportComponent {
 
+  constructor() { }
 
-  @ViewChild("chart") chart : ChartComponent | undefined;
-      public chartOptions: ChartOptions;
+  ngOnInit(){
+
     
-      constructor() {
-        this.chartOptions = {
-          series: [
-            {
-              name: "My Series",
-              data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-            }
-          ],
-          chart: {
-            type: "bar",
-            toolbar: {
-              show: false
-            },
-            height: 350
-          },
-          
-          xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
-          }
-        };
-      }
+  }
+
+  getReportData(){
+    
+  }
+
 
 }
